7fc16fc557664f843f21624ce26ed79b87b000b4
gopeshmit <52699191+gopeshmit@users.noreply.github.com>
2020-03-05 13:02:30 +0530

    Create user.route.js

---
+++ routes/user.route.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const user_controller = require('../controller/user.controller');
+
+
+router.post('/create', user_controller.user_create);
+
+router.get('/', user_controller.user_details);
+
+router.put('/:id/update', user_controller.user_update);
+
+router.delete('/:id/delete', user_controller.user_delete);
+
+module.exports = router;
